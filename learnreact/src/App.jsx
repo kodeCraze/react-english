0111913de75c34c0c2fcdea16add34bfc38ad64b
iAmSahilSharma <146913161+kodeCraze@users.noreply.github.com>
2024-01-08 02:28:24 +0530

    App completed v1.1.1
    v1.1.1 Done

--- learnreact/src/App.jsx
+++ learnreact/src/App.jsx
@@ -1,8 +1,26 @@
-
+import {useState} from 'react'
+import './App.css'
 function App() {
+  const [count, setCount] = useState(0)
+
+  const Reset = () => {
+    setCount(0)
+  }
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decriment = () => {
+    setCount(count - 1)
+  }
   return (
     <>
-      <h1>Counter App</h1>
+    <h1>React Counter By Sahil..</h1>
+      <h2>Click Button</h2>
+      <button onClick={increment}>+</button>
+      <h3>{count}</h3>
+      <button onClick={decriment}>-</button>
+      <p><button onClick={Reset}>Reset</button></p>
     </>
   );
 }
